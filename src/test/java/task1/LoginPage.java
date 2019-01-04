7cf563de2e792866b7284360275d356df1565031
Liudmyla Samko <liuda.samko@gmail.com>
2019-01-04 14:41:04 +0200

    Code refactoring

--- src/test/java/task1/LoginPage.java
+++ src/test/java/task1/LoginPage.java
@@ -1,31 +1,62 @@
 package task1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import java.io.File;
+import java.io.IOException;
 
-public class LoginPage {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", Util.CHROME_BROWSER);
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        String expectedResult = "Welcome To Manager's Page of Guru99 Bank";
-        String message = "Login successful";
-        String actualResult = driver.getTitle();
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-        driver.get(Util.BASE_URL);
-        driver.findElement(By.name("uid")).sendKeys(Util.USER_ID);
-        driver.findElement(By.name("password")).sendKeys(Util.PASSWORD);
-        driver.findElement(By.name("btnLogin")).click();
-
-
-        if (actualResult.contentEquals( expectedResult)) {
-            System.out.println(message);
-        } else {
-            System.out.println("Try again");
-        }
-
-        driver.close();
+public class LoginPage extends DriverSetup {
+    protected LoginPage(WebDriver driver) {
+        super(driver);
     }
+
+    private String actualBoxMsg;
+
+    public void loginPage() {
+
+        driver.findElement(By.name("uid")).clear();
+        driver.findElement(By.name("uid")).sendKeys(Util.USER_ID);
+
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys(Util.PASSWORD);
+
+        driver.findElement(By.name("btnLogin")).click();
+    }
+
+    public void verifyLoginPage() throws IOException {
+        try {
+            Alert alt = driver.switchTo().alert();
+            actualBoxMsg = alt.getText();
+            alt.accept();
+            assertEquals(actualBoxMsg, Util.EXPECT_ERROR);
+        } catch (NoAlertPresentException Ex) {
+            // Get text displays on login page
+            String pageText = driver.findElement(By.tagName("tbody")).getText();
+
+            // Extract the dynamic text mngrXXXX on page
+            String[] parts = pageText.split(Util.PATTERN);
+            String dynamicText = parts[1];
+
+            // Check that the dynamic text is of pattern mngrXXXX
+            // First 4 characters must be "mngr"
+            assertTrue(dynamicText.substring(1, 5).equals(Util.FIRST_PATTERN));
+
+            // remain stores the "XXXX" in pattern mngrXXXX
+            String remain = dynamicText.substring(dynamicText.length() - 4);
+
+            // Check remain string must be numbers;
+            assertTrue(remain.matches(Util.SECOND_PATTERN));
+
+            // Code to take Screenshot
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            // Code to save screenshot at desired location
+            FileUtils.copyFile(srcFile, new File("C:\\Users\\l.samko\\Pictures\\screenshot.png"));
+
+        }
+    }
+
 }
+
--- src/test/java/task1/Util.java
+++ src/test/java/task1/Util.java
@@ -2,9 +2,18 @@ package task1;
 
 public class Util {
     public static final String CHROME_BROWSER = "C:\\Program Files\\maven\\chromedriver.exe";
+    public static final String CHROME_BROWSER1 = "C:\\Users\\Ludmila\\IdeaProjects\\maven\\drivers\\chromedriver.exe";
+
     public static final String USER_ID = "mngr170597";
     public static final String PASSWORD = "quqyveq";
-    public static final String BASE_URL = "http://demo.guru99.com/V4";
 
+    public static final String EXPECT_ERROR = "User or Password is not valid";
+    public static final String EXPECTED_TITLE = "Guru99 Bank Manager HomePage";
 
+    public static final String BASE_URL = "http://demo.guru99.com/";
+    public static final int WAIT_TIME = 30;
+
+    public static final String PATTERN = ":";
+    public static final String FIRST_PATTERN = "mngr";
+    public static final String SECOND_PATTERN = "[0-9]+";
 }
